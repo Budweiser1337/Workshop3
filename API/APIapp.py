4940c5f074652e1bf0b2a61bd4cc90f691387993
engenouadje <e_nouadje@orange.fr>
2024-02-29 19:43:29 +0100

    Update standarized API

--- API/APIapp.py
+++ API/APIapp.py
@@ -1,36 +1,77 @@
 from flask import Flask, request, jsonify
-
+# from sklearn.preprocessing import StandardScaler, OrdinalEncoder
+import os
+import pandas as pd
 import numpy as np
 import joblib
 
 app = Flask(__name__)
 
-model = joblib.load('./github/Models/')
+models_directory = '../Models/'
+loaded_models = {}  # Dictionary to store loaded models
+# numerical_scaler = StandardScaler()
+# categorical_encoder = OrdinalEncoder()
 
-@app.route('/predict', methods=['GET'])
-def predict():
+@app.route('/select_model', methods=['POST', 'GET'])
+def select_model():
     try:
-        # Extract features from the URL parameters
-        features_str = request.args.getlist('features')
-        features = [float(feature) for feature in features_str]
-        # Check if features are provided
-        if not features:
-            raise ValueError("Features not provided in the URL.")
-
-        # Convert features to a numpy array
-        features_array = np.array(features).reshape(1, -1)
-
-        # Make the prediction with the model
-        prediction = model.predict(features_array)
-
-        # Return the prediction to the client
-        return jsonify({'prediction': int(prediction[0])})
+        # Get the model filename from the request
+        model_filename = request.json.get('model')
+        
+        # Check if the model filename is provided
+        if not model_filename:
+            raise ValueError("Model filename not provided.")
+        
+        # Load the selected model if not already loaded
+        if model_filename not in loaded_models:
+            model_path = os.path.join(models_directory, model_filename)
+            loaded_models[model_filename] = joblib.load(model_path)
+        
+        return jsonify({'message': f'Model {model_filename} selected successfully.'})
+    
     except ValueError as ve:
         return jsonify({'error': str(ve)}), 400
     except Exception as e:
         return jsonify({'error': str(e)}), 500
+
+@app.route('/predict', methods=['GET','POST'])
+
+def predict():
+    try:
+        # Extract features from the URL parameters
+        columns = ['pclass','sex',	'age',	'sibsp','parch','fare',	'embarked']
+        features = request.args.getlist('features')
+        features=np.array([features])
+        features=pd.DataFrame(features, columns=columns)
+
+        # Preprocess features
+        # features=np.array(features)
+        
+        # # Check if features are provided
+        # if not features:
+        #     raise ValueError("Features not provided in the URL.")
+
+
+        # Get the selected model
+        selected_model_filename = request.args.get('model')
+        if selected_model_filename not in loaded_models:
+            raise ValueError("Model not selected or loaded. Please select a model using /select_model endpoint.")
+        model = loaded_models[selected_model_filename]
+
+        # Make the prediction with the model
+        prediction = model.predict(features)
+
+        # Return the prediction to the client
+        return jsonify({'prediction': int(prediction[0])})
+    except ValueError as ve:
+
+        return jsonify({'error': str(ve)}), 400
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
 @app.route('/')
 def home():
     return 'Hello, this is the home page!'
+
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
--- API/apiclient.py
+++ API/apiclient.py
@@ -1,18 +1,48 @@
-import random
+
 import requests
 import numpy as np
 
+# Function to select model and get predictions
+def select_models(model_filename):
+    # URL for model selection
+    select_model_url = 'http://127.0.0.1:5000/select_model'
+    headers = {
+    'Content-Type': 'application/json'
+}
+
+    # JSON payload for selecting the model
+    payload = {'model': model_filename}
+
+    # Send a POST request to select the model
+    response = requests.post(select_model_url, json=payload,headers=headers)
+    
+    # Check if model selection was successful
+    if response.status_code != 200:
+        print('Error selecting model:', response.json())
+    else :
+        print('Model selected:', response.json())
+        
+        
+
+    # # URL for prediction
+    # predict_url = 'http://127.0.0.1:5000/predict'
+
+    # # Parameters for prediction request
+    # params = {'features': input_features,'model': model_filename}
+    
+    # # Send a GET request with input features for prediction
+    # response = requests.get(predict_url, params=params)
+    
+    # # Check if prediction request was successful
+    # if response.status_code == 200:
+    #     # prediction = response.json()['prediction']
+    #     print('Prediction:', response.json())
+    #     print('URL:', response.url)
+    # else:
+    #     print('Error getting prediction:', response.json())
 
 
-#URL exemple 
-
-input_features = []
-
-url = 'http://127.0.0.1:5000/predict'
-params = {'features': input_features}
-
-# Making a GET request with URL parameters
-response = requests.get(url, params=params)
-
-print('URL:', response.url)
-
+model_filename = 'RandomForest.pkl'
+# input_features = np.array([[2.0, 'male', 28.0,0.0, 0.0, 13.0, 'S']])
+# get_predictions(model_filename, input_features)
+select_models(model_filename)
